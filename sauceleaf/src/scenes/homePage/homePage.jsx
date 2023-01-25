03ccb4ced5d43b032192f7085ec2ca794c18b3b6
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-25 11:51:34 -0800

    Create homepage structure components

--- sauceleaf/src/scenes/homePage/homePage.jsx
+++ sauceleaf/src/scenes/homePage/homePage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Footer from "../../components/Footer";
+import Hero from "../../components/Hero";
+// import About from "../../components/About";
+// import Hours from "../../components/Hours";
+// import Specials from "../../components/Specials";
+// import Testimonials from "../../components/Testimonials";
+// import Contact from "../../components/Contact";
 import Navbar from "../../components/Navbar";
 
 export default function HomePage() {
   return (
     <div className="main">
       <Navbar />
+      <Hero />
+      {/* <About />
+      <Hours />
+      <Specials />
+      <Testimonials />
+      <Contact /> */}
       <Footer />
     </div>
   );
