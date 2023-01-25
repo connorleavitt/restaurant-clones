7aad303fb6c2b12edd81e7b4e7a00b488be8b286
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-25 12:44:15 -0800

    Add carousel logic and updating image styling

--- sauceleaf/src/components/Hero.jsx
+++ sauceleaf/src/components/Hero.jsx
@@ -1,39 +1,64 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import hero1 from "../../public/img/hero-1.jpg";
 import hero2 from "../../public/img/hero-2.jpg";
 import hero3 from "../../public/img/hero-3.jpg";
 
 export default function Hero() {
-  //     const heroButtons = document.querySelector("[data-carousel-btns]")
-  // function changeHeroImage {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
-  // }
+  console.log(currentSlideIndex);
+
+  const slides = [hero1, hero2, hero3];
+
+  function changeHeroImage(input) {
+    console.log("clicked");
+    if (input === "prev") {
+      console.log("prev");
+      if (currentSlideIndex === 0) {
+        return setCurrentSlideIndex(slides.length - 1);
+      }
+      return setCurrentSlideIndex(currentSlideIndex - 1);
+    } else if (input === "next") {
+      console.log("next");
+      if (currentSlideIndex === slides.length - 1) {
+        return setCurrentSlideIndex(0);
+      }
+      return setCurrentSlideIndex(currentSlideIndex + 1);
+    }
+  }
 
   return (
     <section className="hero-main">
       <div className="hero-image-carousel">
-        <button className="carousel-btn prev" data-carousel-btns>
+        <button
+          onClick={() => {
+            changeHeroImage("prev");
+          }}
+          className="carousel-btn prev"
+        >
           <FontAwesomeIcon
             icon="fa-solid fa-play"
             className="carousel-btn-prev-icon"
           />
         </button>
-        <button className="carousel-btn next" data-carousel-btns>
+        <button
+          className="carousel-btn next"
+          onClick={() => {
+            changeHeroImage("next");
+          }}
+        >
           <FontAwesomeIcon
             icon="fa-solid fa-play"
             className="carousel-btn-next-icon"
           />
         </button>
         <ul>
-          <li className="slide" data-active>
-            <img src={hero1} alt="hero-image-1"></img>
-          </li>
           <li className="slide">
-            <img src={hero2} alt="hero-image-2"></img>
-          </li>
-          <li className="slide">
-            <img src={hero3} alt="hero-image-3"></img>
+            <img
+              src={slides[currentSlideIndex]}
+              alt={`hero-image-${slides[currentSlideIndex]}`}
+            ></img>
           </li>
         </ul>
       </div>
