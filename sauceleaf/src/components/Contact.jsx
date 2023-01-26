860e79099a22f452705bea126724cf0145b0e162
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-26 15:18:10 -0800

    Update contact form styling

--- sauceleaf/src/components/Contact.jsx
+++ sauceleaf/src/components/Contact.jsx
@@ -26,16 +26,43 @@ export default function Contact() {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Subject</label>
-      <input type="subject" name="subject" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Submit" />
-    </form>
+    <section className="contact-section">
+      <div className="contact--header-text">
+        <h2 className="contact--header-text-content">contact</h2>
+      </div>
+      <div className="contact--header-subtext">
+        <p className="contact--header-subtext-content">
+          943 Columbus Ave, San Francisco, CA 94133
+        </p>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="contact--form-container">
+          <input
+            className="contact--form-input name"
+            placeholder="Name"
+            type="text"
+            name="user_name"
+          />
+          <input
+            className="contact--form-input email"
+            placeholder="Email"
+            type="email"
+            name="user_email"
+          />
+          <input
+            className="contact--form-input subject"
+            placeholder="Subject"
+            type="subject"
+            name="subject"
+          />
+          <textarea
+            className="contact--form-input message"
+            placeholder="Message"
+            name="message"
+          />
+        </div>
+        <input className="contact--form-button" type="submit" value="Submit" />
+      </form>
+    </section>
   );
 }
