2901bab288163e091941c5a1808b674f3fb7bc18
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-30 11:15:13 -0800

    Update styling and theming

--- fnn/src/App.jsx
+++ fnn/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "./theme";
+import { createTheme } from "@mui/material/styles";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+
+import HomePage from "./scenes/homePage/homePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const theme = createTheme(themeSettings());
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <HomePage />
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
